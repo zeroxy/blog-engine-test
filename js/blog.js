61b8c4a5d36f0d1878c6defe386d5e42fbf8321b
김광진 <kjink.kim@samsung.com>
2014-08-14 11:33:07 +0900

    이제부터 시작!!

--- js/blog.js
+++ js/blog.js
@@ -1,6 +1,31 @@
+angular.module('blog',['ngRoute'])
+.filter('nonamed', function(){
+	return function(input){return (input == undefined || input == null)?'no-named':input;}
+}).config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+	$routeProvider
+	.when('/', {templateUrl:'posting/'+list[list.length-1], controller:'blogC'})
+	.otherwise({ redirectTo: '/'+list[list.length-1].substring(0,list[list.length-1].lastIndexOf('.')) });
+	for(var i in list){
+		$routeProvider.when('/'+list[i].substring(0,list[i].lastIndexOf('.')), {templateUrl:'posting/'+list[i], controller:'blogC'});
+	}
+}]).controller('blogC', function($scope){
+	$scope.test = 'asdf';
+	$scope.menu = list;
+}).directive('blogmenu', function(){
+	return {
+		templateUrl:'menu.html'
+		, restrict:'E'
+	}
+}).directive('introduction', function(){
+	return {
+		templateUrl:'introduction.html'
+		, restrict:'E'
+	}
+});
+
 var list = [
-{'title':'a1', 'filename':'a1.html'},
-{'title':'a2', 'filename':'a2.html'},
-{'title':'a3', 'filename':'a3.html'},
-{ 'filename':'a4.html'},
-].reverse();
+'a1.html',
+'a2.html',
+'a3.html',
+'a4.html'
+];
