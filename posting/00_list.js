4b6b68338d6d9bd22079a2d38ffd313155ef0ae9
김광진 <kjink.kim@samsung.com>
2014-08-21 10:40:55 +0900

    줄줄이 비엔나 이야기

--- posting/00_list.js
+++ posting/00_list.js
@@ -4,4 +4,5 @@
 ,{file:'2014-08-16.html',tag:'androidpostingtest mobile'}
 ,{file:'markdown.md',tag:'markdown'}
 ,{file:'olleh.md',tag:'markdown 마크다운 성공 개인project'}
+,{file:'TodoList.md',tag:'hobby blog 엔진 만들기 정책 머리아파'}
 ].reverse();
